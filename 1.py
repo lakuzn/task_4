0b5b7ef65807f4fe95c8e086d80c54c14cd4655b
lakuzn <lakuzn003@gmail.com>
2021-10-01 22:53:35 +0300

    last commit

--- 1.py
+++ 1.py
@@ -2,12 +2,12 @@ print("Введите имя студента.")
 a = str(input())
 print("Введите фамилию студента.")
 b = str(input())
-print("Введите дату рождения студента.")
-c = str(input())
-print(a+"_"+b+"_"+c)
+print("Введите год рождения студента")
+c = int(input())
+print(a,b,c,sep="_")
 d = a
 a = b
 b = d
-c = c + "60"
+c = c + 60
 print("Измененные данные:")
-print(a+"_"+b+"_"+c)
+print(a,b,c,sep="_")
